7f22d56bdca85e21c0d03cfc9b737864dc8962f2
Depaula-Dev <roseli.dpaula@gmail.com>
2025-03-05 18:33:53 -0300

    feat: import pygame and configure window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import pygame
 
+print('Setup Start')
+pygame.init()
+
+window = pygame.display.set_mode(size = (600, 480))
+
+print('Loop End')
+
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit() # Close Window
+            quit() # End pygame
