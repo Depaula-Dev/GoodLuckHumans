c106c8232a46bb4215345083787f9c7814807afa
Depaula-Dev <roseli.dpaula@gmail.com>
2025-03-12 22:51:53 -0300

    feat: Insert Player names

--- code/Score.py
+++ code/Score.py
@@ -1,8 +1,10 @@
+import datetime
+import sys
 from tkinter.font import Font
 import pygame
-from pygame import Rect, Surface
-
-from code.const import COLOR_YELLOW, MENU_OPTION, SCORE_POS
+from pygame import K_BACKSPACE, K_RETURN, KEYDOWN, Rect, Surface
+from code.DBProxy import DBProxy
+from code.const import COLOR_WHITE, COLOR_YELLOW, MENU_OPTION, SCORE_POS
 
 
 class Score:
@@ -17,12 +19,44 @@ class Score:
     def save(self, game_mode: str, player_score: list[int]):
         pygame.mixer_music.load('./assets/Score.mp3')
         pygame.mixer_music.play(-1)
-        self.window.blit(source=self.surf, dest=self.rect)
+        db_proxy = DBProxy('DBScore')
+        name = ''
         while True:
+            self.window.blit(source=self.surf, dest=self.rect)
             self.score_text(48, 'YOU WIN!!', COLOR_YELLOW, SCORE_POS['Title'])
             if game_mode == MENU_OPTION[0]:
-                text = 'Player 1 enter your name(4 characters):'
-                
+                score = player_score[0]
+                text = 'Enter Player 1 name(4 characters):'
+            if game_mode == MENU_OPTION[1]:
+                score = (player_score[0] + player_score[1]) /2
+                text = 'Enter Team name (4 characters):'
+            if game_mode == MENU_OPTION[2]:
+                if player_score[0] >= player_score[1]:
+                    score = (player_score[0])
+                    text = 'nter Player 1 name (4 characters):'
+                else:
+                    score = (player_score[1])
+                    text = 'nter Player 2 name (4 characters):'
+
+            self.score_text(20, text, COLOR_WHITE, SCORE_POS['EnterName'])
+
+            # Close
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_RETURN and len(name) == 4:
+                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                        return
+                    elif event.key == K_BACKSPACE:
+                        name = name[:-1]
+                    else:
+                        if len(name) < 4:
+                            name += event.unicode
+            self.score_text(20, name, COLOR_WHITE, SCORE_POS['Name'])
+
             pygame.display.flip()
             pass
 
@@ -42,3 +76,11 @@ class Score:
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
+
+        
+
+def get_formatted_date():
+    current_datetime = datetime.now()
+    current_time = current_datetime.strftime("%H:%M")
+    current_date = current_datetime.strftime("%d/%m/%y")
+    return f"{current_time} - {current_date}"
