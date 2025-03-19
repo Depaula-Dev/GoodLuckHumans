d1d84599c7d2822351d40e7251381ce138523f7e
Depaula-Dev <roseli.dpaula@gmail.com>
2025-03-19 16:12:41 -0300

    feat: remove SatelliteShot

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,23 +1,20 @@
-from code.SatelliteShot import SatelliteShot
-from code.Alien import Alien
+import random
 from code.AlienShot import AlienShot
 from code.Satellite import Satellite
-from code.Entity import Entity
+from code.Alien import Alien
+from code.Background import Background
 from code.Const import WIN_WIDTH
 
 
 class EntityMediator:
 
     @staticmethod
-    def __verify_collision_window(ent: Entity):
+    def __verify_collision_window(ent):
         if isinstance(ent, Satellite):
             if ent.rect.right <= 0:
                 ent.health = 0
         if isinstance(ent, AlienShot):
             if ent.rect.left >= WIN_WIDTH:
-                ent.health = 0
-        if isinstance(ent, SatelliteShot):
-            if ent.rect.right <= 0:
                 ent.health = 0
 
     @staticmethod
@@ -27,21 +24,17 @@ class EntityMediator:
             valid_interection = True
         elif isinstance(ent1, AlienShot) and isinstance(ent2, Satellite):
             valid_interection = True
-        elif isinstance(ent1, Alien) and isinstance(ent2, SatelliteShot):
-            valid_interection = True
-        elif isinstance(ent1, SatelliteShot) and isinstance(ent2, Alien):
-            valid_interection = True
 
-        if valid_interection: 
+        if valid_interection:
             if (ent1.rect.right >= ent2.rect.left and ent1.rect.left <= ent2.rect.right and
-                ent1.rect.bottom >= ent2.rect.top and ent1.rect.top <=ent2.rect.bottom):
+                ent1.rect.bottom >= ent2.rect.top and ent1.rect.top <= ent2.rect.bottom):
                 ent1.health -= ent2.damage
                 ent2.health -= ent1.damage
                 ent1.last_dmg = ent2.name
                 ent2.last_dmg = ent1.name
     
     @staticmethod
-    def __give_score(enemy: Satellite, entity_list: list[Entity]):
+    def __give_score(enemy: Satellite, entity_list):
         if enemy.last_dmg == 'Alien1Shot':
             for ent in entity_list:
                 if ent.name == 'Alien1':
@@ -51,10 +44,8 @@ class EntityMediator:
                 if ent.name == 'Alien2':
                     ent.score += enemy.score
 
-
-
     @staticmethod
-    def verify_collision(entity_list: list[Entity]):
+    def verify_collision(entity_list):
         for i in range(len(entity_list)):
             entity1 = entity_list[i]
             EntityMediator.__verify_collision_window(entity1)
@@ -63,7 +54,7 @@ class EntityMediator:
                 EntityMediator.__verify_collision_entity(entity1, entity2)
 
     @staticmethod
-    def verify_health(entity_list: list[Entity]):
+    def verify_health(entity_list):
         for ent in entity_list:
             if ent.health <= 0:
                 if isinstance(ent, Satellite):
--- code/Satellite.py
+++ code/Satellite.py
@@ -1,23 +1,9 @@
-
-
-from code.SatelliteShot import SatelliteShot
 from code.Entity import Entity
-from code.Const import ENTITY_SHOT_DELAY, ENTITY_SPEED
-
+from code.Const import ENTITY_SPEED
 
 class Satellite(Entity):
-    
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
-        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
-    
-    def shot(self):
-        self.shot_delay -= 1
-        if self.shot_delay == 0:
-            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
-            return SatelliteShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
-
-
